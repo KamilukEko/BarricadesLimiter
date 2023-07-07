6410aaf06a16f12ae6132f8c7e43714718688111
Kamil <kamiluk@onet.pl>
2023-07-07 06:12:23 +0200

    Initial commit

---
+++ BarricadesLimiter/Config.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Rocket.API;
+
+namespace BarricadeLimiter
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public List<ushort> WhitelistedBarricades;
+
+        public void LoadDefaults()
+        {
+            WhitelistedBarricades = new List<ushort> {1096};
+        }
+    }
+}
---
+++ BarricadesLimiter/Main.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using Rocket.Core.Plugins;
+using SDG.Unturned;
+using UnityEngine;
+using Logger = Rocket.Core.Logging.Logger;
+
+namespace BarricadeLimiter
+{
+    public class Main : RocketPlugin<Config>
+    {
+        protected override void Load()
+        {
+            BarricadeManager.onDeployBarricadeRequested += OnDeployBarricadeRequested;
+            
+            Logger.Log($"Kamiluk || BarricadeLimiter plugin has been loaded");
+        }
+
+        private void OnDeployBarricadeRequested(Barricade barricade, ItemBarricadeAsset asset, Transform hit, ref Vector3 point, ref float angle_x, ref float angle_y, ref float angle_z, ref ulong owner, ref ulong group, ref bool shouldallow)
+        {
+            if (hit.GetComponent<InteractableVehicle>() == null)
+                return;
+            
+            if (Configuration.Instance.WhitelistedBarricades.Contains(asset.id))
+                return;
+
+            shouldallow = false;
+        }
+
+        protected override void Unload()
+        {
+            BarricadeManager.onDeployBarricadeRequested -= OnDeployBarricadeRequested;
+            
+            Logger.Log($"Kamiluk || BarricadeLimiter plugin has been unloaded");
+        }
+    }
+}
